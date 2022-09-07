93d9341c4067b381b2ebeca2fd9eec0a28d61367
Coko05 <112122648+Coko05@users.noreply.github.com>
2022-09-06 19:42:18 -0500

    Entrega 1 Bootcamp

---
+++ BootCampJLS-Entrega1/src/com/companyTest/TiposDatos.java
@@ -0,0 +1,23 @@
+package com.companyTest;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+
+
+        char caracter = 'j';
+        int numeroInt  = 1000;
+        long numeroLong    =  1685545454;
+        float  numerofloat =5545454;
+        double numerodouble = 1.669d;
+        boolean meGustaLaProgramacion = true;
+        String  dataString = "Jorge Silva";
+
+
+
+        
+
+
+
+    }
+}
